c50fb5e598b365dcf414b06390c18af26a44e48d
Luis Tirado <luis.tirado@axity.com>
2025-01-02 18:07:00 -0600

    Cambiar javax.persistence por jakarta.persistence en proyecto de Java 17

--- Back/ATV-Backend/office-service/axity.office-model/src/main/java/com/axity/office/model/AccessByUser.java
+++ Back/ATV-Backend/office-service/axity.office-model/src/main/java/com/axity/office/model/AccessByUser.java
@@ -1,6 +1,6 @@
 package com.axity.office.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "ATVFFUSA")
--- Back/ATV-Backend/office-service/axity.office-model/src/main/java/com/axity/office/model/ProductByUser.java
+++ Back/ATV-Backend/office-service/axity.office-model/src/main/java/com/axity/office/model/ProductByUser.java
@@ -3,10 +3,7 @@ package com.axity.office.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "ATVFFUSP")
