df23b5cc6ed4fbae2ab5ad7461b1416e0f82a2d2
harold1982 <Harold.Castillo@axity.com>
2025-01-08 20:42:53 -0500

    Ajustes en el metodo de actualizacion de branches por usuario

--- Back/ATV-Backend/office-service/axity.office-persistence/src/main/java/com/axity/office/persistence/BranchByUserRepository.java
+++ Back/ATV-Backend/office-service/axity.office-persistence/src/main/java/com/axity/office/persistence/BranchByUserRepository.java
@@ -1,11 +1,13 @@
 package com.axity.office.persistence;
 
 import com.axity.office.model.BranchByUser;
-import com.axity.office.model.ProductByUser;
+
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,6 +18,8 @@ public interface BranchByUserRepository extends JpaRepository<BranchByUser, Long
     @Query("SELECT u FROM BranchByUser u WHERE u.xsuser = :userId ORDER BY u.xsid ASC NULLS LAST")
     List<BranchByUser> findAllByUser(@Param("userId") String userId);
 
+    @Modifying
+    @Transactional
     @Query("DELETE FROM BranchByUser u WHERE u.xsuser = :userId AND u.xscosu = :branchId")
     void deleteByUserAndBranch(@Param("userId") String userId, @Param("branchId") Short branchId);
 }
